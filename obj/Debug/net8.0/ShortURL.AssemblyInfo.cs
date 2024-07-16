ffb3a0894d9b90e6d3b81b0c5bd124cfb076e1f1
ricardocarva <ricardofecarva@gmail.com>
2024-07-16 19:51:45 -0400

    Fixed clear and submit buttons. Hid unused components

--- obj/Debug/net8.0/ShortURL.AssemblyInfo.cs
+++ obj/Debug/net8.0/ShortURL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShortURL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+989d620f3f124bc63ee76ad26a8d19bcda996c6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShortURL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShortURL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
