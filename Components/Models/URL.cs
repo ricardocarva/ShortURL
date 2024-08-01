d9a0f90025afb9a9991662f57ec01d5b0e52a344
Gustavo Bastos <158234660+Tav0nes@users.noreply.github.com>
2024-07-31 21:50:03 -0400

    Revert "URL Shortener implemented"

--- Components/Models/URL.cs
+++ Components/Models/URL.cs
@@ -1,10 +1,7 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace ShortURL;
 
 public class URL
 {
-    public string OriginalURL { get; set; } = string.Empty;
-    public string ShortenedUrl { get; set; } = string.Empty;
-
+    public string originalURL { get; set; } = string.Empty;
+    public string shortenedUrl { get; set; } = string.Empty;
 }
