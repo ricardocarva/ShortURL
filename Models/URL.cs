193291274f7b71b3cd47fcde3284cf7302e47e71
Gustavo Bastos <158234660+Tav0nes@users.noreply.github.com>
2024-07-31 21:49:55 -0400

    Merge pull request #5 from Tav0nes/HTTPImplementation
    URL Shortener implemented

---
+++ Models/URL.cs
@@ -0,0 +1,10 @@
+namespace ShortURL.Models
+{
+
+    public class URL
+    {
+        public string OriginalURL { get; set; } = string.Empty;
+        public string ShortenedUrl { get; set; } = string.Empty;
+
+    }
+}
